e78c9f0e94593dbc8300896130ed539a3241fb40
ns8468 <82871834+ns8468@users.noreply.github.com>
2021-09-02 19:57:46 +0530

    Create solution.sql

---
+++ SQL/Basic Select/Weather Observation Station 6/solution.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT CITY
+FROM STATION
+WHERE 
+CITY LIKE 'a%' 
+or CITY  LIKE 'e%' 
+or CITY LIKE 'i%' 
+or CITY LIKE 'o%' 
+or CITY LIKE 'u%'
+or CITY LIKE 'A%' 
+or CITY LIKE 'E%' 
+or CITY LIKE 'I%' 
+or CITY LIKE 'O%'
+or CITY  LIKE 'U%' ;
