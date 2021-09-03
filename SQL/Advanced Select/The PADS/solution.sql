da685070115d3596b2422b8dfcac539d46f50ae0
ns8468 <82871834+ns8468@users.noreply.github.com>
2021-09-04 01:36:48 +0530

    Update solution.sql

--- SQL/Advanced Select/The PADS/solution.sql
+++ SQL/Advanced Select/The PADS/solution.sql
@@ -1,1 +1,3 @@
 
+SELECT CONCAT(Name, '(', SUBSTR(Occupation,1,1),')') FROM OCCUPATIONS ORDER BY Name;
+SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation), 's.') FROM OCCUPATIONS GROUP BY Occupation ORDER BY COUNT(Occupation), Occupation;
