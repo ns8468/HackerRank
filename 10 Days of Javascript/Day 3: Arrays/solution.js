8175e532a6436008729689fda97083a9e8a46a22
Nihal Shah Parekkattil <82871834+ns8468@users.noreply.github.com>
2021-09-04 11:11:30 +0530

    Create solution.js

---
+++ 10 Days of Javascript/Day 3: Arrays/solution.js
@@ -0,0 +1,8 @@
+function getSecondLargest(nums) {
+    // Complete the function
+    const max = Math.max(...nums);
+
+    nums = nums.filter(num => num !== max);
+
+    return Math.max(...nums);
+}
