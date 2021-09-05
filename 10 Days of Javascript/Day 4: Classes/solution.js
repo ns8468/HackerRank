f18375a29ba09a7ebe393c2fbb6885547feceeb4
Nihal Shah Parekkattil <82871834+ns8468@users.noreply.github.com>
2021-09-05 18:50:34 +0530

    Update solution.js

--- 10 Days of Javascript/Day 4: Classes/solution.js
+++ 10 Days of Javascript/Day 4: Classes/solution.js
@@ -1,1 +1,10 @@
 
+class Polygon {
+    constructor(args) {
+        this.values = args;
+    }
+
+    perimeter() {
+        return (this.values || []).reduce((target, item) => target + item);
+    }
+}
