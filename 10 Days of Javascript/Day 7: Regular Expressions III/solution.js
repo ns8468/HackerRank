c32e0f76bf6421cc35e48fb5686da4ac3f906705
Nihal Shah Parekkattil <82871834+ns8468@users.noreply.github.com>
2021-09-08 19:48:29 +0530

    Create solution.js

---
+++ 10 Days of Javascript/Day 7: Regular Expressions III/solution.js
@@ -0,0 +1,14 @@
+
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    const re = new RegExp(/[0-9]+/gm);
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
